c02ba90ba3838fbf11018e985f92406582b72caf
Rebecca Tan <rebecca_tan_97@hotmail.com>
2019-07-26 09:26:56 +0800

    Update event_finder.py

--- event_finder.py
+++ event_finder.py
@@ -17,16 +17,4 @@ class EventFinder(object):
         if json_result == []:
             return None
         return json_result[0]
-
-    def containsMovementEvent(self, src, dst, port, timestamp):
-        search_str = 'python search.py "search '
-        query = search_str + "DESCRIPTION=* " + "SRCHOST=" + src + " DSTHOST=" + dst + " DSTPORT=" + str(port) + " TIMESTAMP<" + str(timestamp)
-        query += '" --username="Imzai" --password="password" --output_mode=json'
-        # print(query)
-        os.chdir("splunk/examples")
-        status, result = subprocess.getstatusoutput(query)
-        json_result = json.loads(result)["results"]
-        os.chdir("../..")
-        if json_result == []:
-            return None
-        return json_result[0]
+    
