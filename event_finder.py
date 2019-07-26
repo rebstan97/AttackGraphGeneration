dd71079bad492a906fe8fcd3be489246f2215248
Rebecca Tan <rebecca_tan_97@hotmail.com>
2019-07-26 16:21:04 +0800

    Add code for configuring splunk

--- event_finder.py
+++ event_finder.py
@@ -3,12 +3,15 @@ import os
 import subprocess
 
 class EventFinder(object):
+    def __init__(self, username, password):
+        self.splunk_username = username
+        self.splunk_password = password
 
     # Checks if a vulnerability event is present in the event set
     def containsVulnEvent(self, description, host, port, timestamp):
         search_str = 'python search.py "search '
         query = search_str + description + " SRCHOST=*" + " DSTHOST=" + host + " DSTPORT=" + str(port) + " TIMESTAMP<" + str(timestamp)
-        query += '" --username="Imzai" --password="password" --output_mode=json'
+        query += '" --username="' + self.splunk_username + '" --password="' + self.splunk_password + '" --output_mode=json'
         # print(query)
         os.chdir("splunk/examples")
         status, result = subprocess.getstatusoutput(query)
@@ -17,3 +20,4 @@ class EventFinder(object):
         if json_result == []:
             return None
         return json_result
+    
